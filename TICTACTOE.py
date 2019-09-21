08db06afd4e02def722ca87e7be81707cb30066e
StephanGuingor <53459789+StephanGuingor@users.noreply.github.com>
2019-09-21 01:13:15 -0500

    TicTac.py
    Best

--- TICTACTOE.py
+++ TICTACTOE.py
@@ -1,6 +1,6 @@
-﻿import random
+from random import randrange
+from random import choice
 
-board = [[1,2,3],[4,5,6],[7,8,9]]
 
 def DisplayBoard(board):
     plus = "+"
@@ -21,8 +21,6 @@ def DisplayBoard(board):
                 print(up+(blank*3)+str(board[2][0])+(blank*3)+up+(blank*3)+str(board[2][1])+(blank*3)+up+(blank*3)+str(board[2][2])+(blank*3)+up)   
         elif i%2==0:
             print((up+(blank*7))*3+up)
-
-
     
 # the function accepts one parameter containing the board's current status
 # and prints it out to the console
@@ -30,23 +28,27 @@ def DisplayBoard(board):
 
 def EnterMove():
     global board
-    while True:
-        answer = int(input("Enter your move: "))
-        if answer >=1 and answer <= 9:
-            if answer in MakeListOfFreeFields(board):
-                if answer == 1 or answer ==2 or answer ==3:
-                    board[0][answer-1]= "O"
-                    break
-                elif answer ==4 or answer ==5 or answer ==6:
-                    answer = answer - 3
-                    board[1][answer-1]="O"
-                    break
-                elif answer ==7 or answer ==8 or answer ==9:
-                    answer = answer - 6
-                    board[2][answer-1]="O"
-                    break
-        else:
-            print("Enter valid input\n")
+    if VictoryFor(board,"X") and VictoryFor(board,"O"):
+        while True:
+            answer = int(input("Enter your move: "))
+            if answer >=1 and answer <= 9:
+                if answer in MakeListOfFreeFields(board):
+                    if answer == 1 or answer ==2 or answer ==3:
+                        board[0][answer-1]= "O"
+                        DisplayBoard(board)
+                        break
+                    elif answer ==4 or answer ==5 or answer ==6:
+                        answer = answer - 3
+                        board[1][answer-1]="O"
+                        DisplayBoard(board)
+                        break
+                    elif answer ==7 or answer ==8 or answer ==9:
+                        answer = answer - 6
+                        board[2][answer-1]="O"
+                        DisplayBoard(board)
+                        break
+            else:
+                print("Enter valid input\n")
             
 # the function accepts the board current status, asks the user about their move, 
 # checks the input and updates the board according to the user's decision
@@ -56,7 +58,7 @@ def MakeListOfFreeFields(board):
     available = []
     for row in board:
         for square in row:
-            if not square == "X":
+            if not square == "X" and square != "O" :
                 available.append(square)
     return available
             
@@ -64,23 +66,89 @@ def MakeListOfFreeFields(board):
 # the function browses the board and builds a list of all the free squares; 
 # the list consists of tuples, while each tuple is a pair of row and column numbers
 #
-
+ 
 def VictoryFor(board, sign):
-    pass
+    for i in range(3):
+        if sign == board[i][0] and sign == board[i][1] and sign ==board[i][2]:
+            print(sign + "s"+" wins!")
+            return False
+        if sign == board[0][2] and sign ==board[1][2] and sign==board[2][2]:
+            print(sign + "s"+" wins!")
+            return False
+        if sign == board[0][1] and sign ==board[1][1] and sign==board[2][1]:
+            print(sign + "s"+" wins!")
+            return False
+        if sign == board[0][0] and sign ==board[1][0] and sign==board[2][0]:
+            print(sign + "s"+" wins!")
+            return False
+
+        if sign == board[0][0] and sign ==board[1][1] and sign==board[2][2]:
+            print(sign + "s"+" wins!")
+            return False
+        if sign == board[0][2] and sign ==board[1][1] and sign==board[2][0]:
+            print(sign + "s"+" wins!")
+            return False
+
+        elif not (1 in MakeListOfFreeFields(board) and 2 in MakeListOfFreeFields(board) or 3 in MakeListOfFreeFields(board) or 4 in MakeListOfFreeFields(board)\
+            or 5 in MakeListOfFreeFields(board) or 6 in MakeListOfFreeFields(board) or 7 in MakeListOfFreeFields(board) \
+            or 8 in MakeListOfFreeFields(board) or 9 in MakeListOfFreeFields(board)):
+            print("Tie")
+            return False
+        else:
+            return True
+   
+    
 #
 # the function analyzes the board status in order to check if 
 # the player using 'O's or 'X's has won the game
 #
 
-def DrawMove(board):
+def DrawMove():
+    if VictoryFor(board,"X") and VictoryFor(board,"O"):
+        answer = choice(MakeListOfFreeFields(board))
+        while True:
+            if answer >=1 and answer <= 9:
+                if answer in MakeListOfFreeFields(board):
+                    if answer == 1 or answer ==2 or answer ==3:
+                        board[0][answer-1]= "X"
+                        DisplayBoard(board)
+                        break
+                    elif answer ==4 or answer ==5 or answer ==6:
+                        answer = answer - 3
+                        board[1][answer-1]="X"
+                        DisplayBoard(board)
+                        break
+                    elif answer ==7 or answer ==8 or answer ==9:
+                        answer = answer - 6
+                        board[2][answer-1]="X"
+                        DisplayBoard(board)
+                        break
+            else:
+                listx = MakeListOfFreeFields(board)
+                print(listx)
+                answer = randrange(9)
+                if answer in MakeListOfFreeFields(board):
+                    continue
+                else:
+                    answer = randrange(9)
+                    if answer in MakeListOfFreeFields(board):
+                        continue
     
-    pass
-#
+  
+
 # the function draws the computer's move and updates the board
 #
+def MainLoop(board,sign1,sign2):
+    print("Welcome to TIC-TAC-TOE")
+    while True:
+        DisplayBoard(board)    
+        EnterMove()
+        if not (VictoryFor(board, sign1) and VictoryFor(board, sign2)):
+            break
+        DrawMove()
+        if not (VictoryFor(board, sign1) and VictoryFor(board, sign2)):
+            break
+#Calling the GAME
+board = [[1,2,3],[4,5,6],[7,8,9]]
+MainLoop(board,"X","O")
 
-#Main Loop
-print("Welcome to TIC-TAC-TOE")
-DisplayBoard(board)
-EnterMove()
-print(board)
